e3eeb742cb0114f258b179f8bf6ccb7ebe524e97
Rahul Agrawal Bejarano <rahulab@umich.edu>
2018-03-07 14:56:34 -0500

    making sure everything is up to date

--- rpn.py
+++ rpn.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python 3
+
 import operator
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
 
 operators = {
     '+': operator.add,
@@ -6,8 +15,16 @@ operators = {
     '*': operator.mul,
     '/': operator.truediv,
     '^': operator.pow,
+    '&': operator.and_,
+    '|': operator.or_,
+    '~': operator.inv,
+    '!': operator.mul,
 }
+
+prev = 0
+
 def calculate(arg):
+    global prev
     stack = list()
     for token in arg.split():
         try:
@@ -15,10 +32,26 @@ def calculate(arg):
             stack.append(value)
         except ValueError:
             function = operators[token]
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
+            if (token == '~'):
+                arg2 = stack.pop()
+                result = function(arg2)
+            elif(token == '!'):
+                arg2 = stack.pop()
+                result = 1
+                while (arg2 > 1):
+                    result = function(result, arg2)
+                    arg2 = arg2 - 1
+            else:
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                try:
+                    result = function(arg1, arg2)
+                except ZeroDivisionError:
+                    print('You tried to divide by zero :(')
+                    result = prev
+            prev = result
             stack.append(result)
+        logger.debug(stack)
         print(stack)
 
     if (len(stack) != 1):
--- test_rpn.py
+++ test_rpn.py
@@ -23,3 +23,26 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate('2 3 ^')
         self.assertEqual(8, result)
+    def test_bitwiseAND(self):
+        result = rpn.calculate('15 11 &')
+        self.assertEqual(11, result)
+    def test_bitwiseOR(self):
+        result = rpn.calculate('15 22 |')
+        self.assertEqual(31, result)
+        result = rpn.calculate('22 0 |')
+        self.assertEqual(22, result)
+    def test_bitwiseNOT(self):
+        result = rpn.calculate('1 ~')
+        self.assertEqual((-2), result)
+        result = rpn.calculate('0 ~')
+        self.assertEqual(-1, result)
+        result = rpn.calculate('3 ~')
+        self.assertEqual((-4), result)
+    def test_factorial(self):
+        result = rpn.calculate('4 !')
+        self.assertEqual(24, result)
+        result = rpn.calculate('0 !')
+        self.assertEqual(1, result)
+    def test_divisionZero(self):
+        result = rpn.calculate('1 0 /')
+        self.assertEqual(3, result)
